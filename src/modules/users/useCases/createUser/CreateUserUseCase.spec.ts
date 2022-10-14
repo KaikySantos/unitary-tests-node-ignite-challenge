bf32ef8fe358ebc41ba9a243cfa85307b92d25ee
kaikySantos <info.kaiky@gmail.com>
2022-10-14 20:26:55 -0300

    feat: create user test

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,22 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let usersRepositoryInMemory: InMemoryUsersRepository;
+
+describe("Create user", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("should be able to create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "kaiky",
+      email: "carlos.kaiky62@gmail.com",
+      password: "12345"
+    });
+
+    expect(user).toHaveProperty("id");
+  });
+});
